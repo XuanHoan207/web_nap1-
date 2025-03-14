4a04efbca72308462feea3e2c3da97cce31c83e8
XuanHoan207 <151051643+XuanHoan207@users.noreply.github.com>
2025-03-14 13:54:35 +0700

    up code

---
+++ Java/demojava/src/demojava/Hellojava.java
@@ -0,0 +1,8 @@
+package demojava;
+
+public class Hellojava {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("hello ");
+    }
+}
